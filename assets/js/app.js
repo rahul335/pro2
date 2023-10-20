c6659a27a6ec7fe1c3428ea83241a5c045fb5fc3
rahul335 <74925492+rahul335@users.noreply.github.com>
2023-10-21 00:48:05 +0530

    corsion

---
+++ assets/js/app.js
@@ -0,0 +1,104 @@
+var cl = console.log;
+
+// var x = 10;
+// var y = 20;
+
+// cl(x, typeof x)
+// cl(y, typeof y)
+
+// var q = '30';
+// cl(q, typeof q)
+
+
+// var z = x + y;
+// cl(z, typeof z);
+
+// var z = x + q; // in this case concatination will happend
+// cl(z, typeof z); // in this case JS convert number into string
+
+
+// var z = x - q; // in this case JS convert string in to number
+// cl(z, typeof z);
+
+// var z = x - y;
+// cl(z, typeof z);
+
+// var z = x * q;
+// cl(z, typeof z);
+
+// var z = x * y;
+// cl(z, typeof z);
+
+// var z = x / y;
+// cl(z, typeof z)
+
+
+// var a = 50;
+// var b = 10;
+
+// var z = a / b;
+// cl(z, typeof z)
+
+// var z = a % b;
+// cl(z, typeof z);
+
+
+// var a = 10;
+// var b = 10;
+// var x;
+// var c = '10';
+// var e = 10;
+// var y;
+// var f = 10;
+// var p = true;
+
+
+
+// var result = a + b + y + c + e + p;
+
+//2010undefine10true
+
+// cl(result, typeof result);
+
+
+
+// var c = 1;
+// var v = ++c + ++c + ++c
+// var v1 = c++ + ++c + c++
+
+//post Increment (i++) : current value of 'i' is used and then it is incremented by 1
+
+//post Decrement (i--) : current value of 'i' is used and then it is decremented by 1
+
+
+//pre Increment (++i) : first 'i' is incremented by 1 and then it's value is used
+
+//pre decrement (--i) : first 'i' is decremented by 1 and then it's value is used
+
+
+
+
+
+
+
+
+// var result = a + x; // 10+undefine
+
+// cl(result, typeof result);
+
+// var result1 = x + c;
+// cl(result1, typeof result1)
+
+// var result02 = c + p;
+// cl(result02, typeof result02)
+
+// var z = a + p;
+// cl(z, typeof z)
+
+
+
+ 
+
+
+
+
